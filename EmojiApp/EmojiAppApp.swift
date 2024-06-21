d3e0b87cf1beefc44acba64495b7379b00aaa1d6
avan36 <158532304+avan36@users.noreply.github.com>
2024-06-21 23:58:30 +0100

    Initial Commit

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Ambrose V on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
