34044aa2661c37eb5bc368f73c359b4d830bf288
avan36 <158532304+avan36@users.noreply.github.com>
2024-06-22 13:10:44 +0100

    V1

--- EmojiApp/ContentView.swift
+++ EmojiApp/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    let happy = ["😊", "😍", "😂", "😎", "🥰", "😃", "😁", "😄", "😆", "😅", "🤗", "😇"]
+    let activities = ["⚽", "🎸", "🎨", "🎮", "📚", "🏀", "🏈", "🏐", "🏓", "🎯", "🏹", "🛹"]
+    let hand_expressions = ["👍", "🙌", "🤗", "🤔", "🙏", "👏", "✌️", "👌", "🤝", "💪", "👋", "🤞"]
+    let food = ["🍕", "🍔", "🍦", "🍓", "🍩", "🍎", "🍉", "🍇", "🍌", "🍒", "🍪", "🍫"]
+    let sad = ["😢", "😭", "😞", "😟", "😔", "😣", "😖", "😫", "😩", "😢", "🥺", "😿"]
+    let angry = ["😡", "😤", "😠", "😣", "😖", "🤬", "😾", "😬", "😣", "😤", "😡", "😠"]
+    let neutral = ["😐", "😶", "😕", "😒", "😔", "😑", "😌", "😇", "😏", "😬", "😐", "😶"]
+    let animals = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐨", "🐯", "🦁", "🐮"]
+    let travel = ["✈️", "🚗", "🚀", "🚁", "🚂", "🚤", "🛳️", "🚲", "🛴", "🏍️", "🚜", "🚕"]
+    let weather = ["☀️", "🌧️", "⛈️", "❄️", "🌨️", "🌩️", "🌪️", "🌫️", "🌬️", "🌈", "☁️", "☔"]
+    let options = ["Happy", "Sad", "Angry", "Neutral", "Activities", "Hand Expressions", "Food", "Animals", "Travel", "Weather"]
+    
+    @State private var selectedCategory = "Happy"
+    
+    var emojiDictionary: [String: [String]] {
+        return [
+            "Happy": happy,
+            "Sad": sad,
+            "Angry": angry,
+            "Neutral": neutral,
+            "Activities": activities,
+            "Hand Expressions": hand_expressions,
+            "Food": food
+        ]
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Picker("Emoji category", selection: $selectedCategory) {
+                    ForEach(options, id: \.self) {
+                        Text($0)
+                        }
+                } //:Picker
+                
+                Section("Tap an emoji to copy to clipboard"){
+                    if let emojis = emojiDictionary[selectedCategory] {
+                        ForEach(emojis, id: \.self) {emoji in
+                            Button(action: {
+                                UIPasteboard.general.string = emoji
+                            }, label: {
+                                Text(emoji)
+                            })
+                        }//:EMOJI DICTIONARY
+                    }
+                }//:Section
+                
+                
+            }
+            .navigationTitle("Fast Emoji Picker")
         }
-        .padding()
     }
 }
 
